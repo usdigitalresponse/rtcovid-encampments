acef53507efe99bfaa24368c24447a068e9f5615
M. Jackson Wilkinson <jackson.wilkinson@pathai.com>
2020-05-21 22:34:58 -0400

    Black formatting

--- apps/reporting/management/commands/bootstrap_location.py
+++ apps/reporting/management/commands/bootstrap_location.py
@@ -10,19 +10,20 @@ from django.db import connection
 from apps.reporting import models as reporting_models
 
 PREREQUISITE_COMMANDS = {
-    'oakland': [
-        'preload_oakland',
-    ],
+    "oakland": ["preload_oakland",],
 }
 
-ordinal = lambda n: "%d%s"%(n,{1:"st",2:"nd",3:"rd"}.get(n if n<20 else n%10,"th"))
+ordinal = lambda n: "%d%s" % (
+    n,
+    {1: "st", 2: "nd", 3: "rd"}.get(n if n < 20 else n % 10, "th"),
+)
 
 
 class Command(BaseCommand):
     help = "Bootstrap a dev environment for a particular city"
 
     def add_arguments(self, parser):
-        parser.add_argument('city', nargs=1, type=str)
+        parser.add_argument("city", nargs=1, type=str)
 
     def letter_from_int(self, num):
         num_alpha = dict(zip(range(1, 27), string.ascii_lowercase))
@@ -39,7 +40,7 @@ class Command(BaseCommand):
 
     def make_random_location(self, count):
         # Trying to be fun, I guess
-        suffices = ['Ave', 'St', 'Blvd', 'Rd', 'Rt']
+        suffices = ["Ave", "St", "Blvd", "Rd", "Rt"]
         if count % 2:
             result = "{} {} {}.".format(
                 random.randrange(1, 99999),
@@ -47,7 +48,9 @@ class Command(BaseCommand):
                 suffices[count % len(suffices)],
             )
         else:
-            st1 = "{} {}.".format(self.letter_from_int(count).upper(), random.choice(suffices))
+            st1 = "{} {}.".format(
+                self.letter_from_int(count).upper(), random.choice(suffices)
+            )
             st2 = "{} {}.".format(ordinal(count), random.choice(suffices))
             result = "{} at {}".format(st1, st2)
         self.stdout.write(self.style.HTTP_INFO("Created encampment: {}".format(result)))
@@ -56,7 +59,7 @@ class Command(BaseCommand):
     def make_random_encampments(self):
         count = 0
         for r in reporting_models.Region.objects.all():
-            sql = 'SELECT ST_AsEWKT(ST_GeneratePoints(ST_GeomFromEWKT(\'{ewkt}\'), 4))'
+            sql = "SELECT ST_AsEWKT(ST_GeneratePoints(ST_GeomFromEWKT('{ewkt}'), 4))"
             cursor = connection.cursor()
             cursor.execute(sql.format(ewkt=r.geom.wkt))
             points = GEOSGeometry(cursor.fetchall()[0][0])
@@ -75,7 +78,7 @@ class Command(BaseCommand):
             call_command(command)
 
     def handle(self, *args, **options):
-        city = options['city'][0]
+        city = options["city"][0]
         self.stdout.write("Bootstrapping for {}".format(city))
         self.run_prerequisites(city)
         self.make_random_encampments()
